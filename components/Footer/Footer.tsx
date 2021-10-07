95effe32d7ee5d3d91ab49a85ecb22616c765c64
Seungchan <scjin91@gmail.com>
2021-10-07 20:59:52 +0900

    feat: footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,4 +1,9 @@
+import Link from "next/link";
+import { Facebook } from "../icons/Facebook";
+import { Instagram } from "../icons/Instagram";
 import { Logo } from "../icons/Logo";
+import { Pinterest } from "../icons/Pinterest";
+import { Twitter } from "../icons/Twitter";
 import * as S from "./styles";
 
 export const Footer = () => {
@@ -8,6 +13,33 @@ export const Footer = () => {
         <S.Logo>
           <Logo />
         </S.Logo>
+        <S.Nav>
+          <S.NavList>
+            <S.NavItem>
+              <Link href="/">
+                <a>About</a>
+              </Link>
+            </S.NavItem>
+            <S.NavItem>
+              <Link href="/">
+                <a>Services</a>
+              </Link>
+            </S.NavItem>
+            <S.NavItem>
+              <Link href="/">
+                <a>Projects</a>
+              </Link>
+            </S.NavItem>
+          </S.NavList>
+        </S.Nav>
+        <S.SNSContainer>
+          <S.SNSGroup>
+            <Facebook />
+            <Instagram />
+            <Twitter />
+            <Pinterest />
+          </S.SNSGroup>
+        </S.SNSContainer>
       </S.Container>
     </S.Footer>
   );
--- components/Footer/styles.ts
+++ components/Footer/styles.ts
@@ -11,17 +11,60 @@ export const Footer = styled.footer`
 `;
 
 export const Container = styled.div`
-  width: ${rem(297)};
+  width: ${rem(300)};
   height: ${rem(206)};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 export const Logo = styled.div`
   text-align: center;
 
   svg {
-    /* transform: scale(${170 / 124}); */
     path {
       fill: ${({ theme }) => theme.palette.dark_cyan};
+    }
+  }
+`;
+
+export const Nav = styled.nav`
+  width: 100%;
+`;
+
+export const NavList = styled.ul`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const NavItem = styled.li`
+  a {
+    font-size: ${rem(18)};
+    color: ${({ theme }) => theme.palette.dark_moderate_cyan};
+
+    &:hover {
+      color: ${({ theme }) => theme.palette.white};
+    }
+  }
+`;
+
+export const SNSContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const SNSGroup = styled.ul`
+  & > * + * {
+    margin-left: ${rem(28)};
+  }
+
+  svg {
+    cursor: pointer;
+
+    &:hover {
+      path {
+        fill: ${({ theme }) => theme.palette.white};
+      }
     }
   }
 `;
